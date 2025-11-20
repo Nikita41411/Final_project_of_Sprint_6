b0695915cef9ed7db8e4cad89feb639f5c8baff0
Nikita Kukushkin <kukushkin.n.a@yandex.ru>
2025-11-20 09:08:14 +0300

    Исправление замечаний на ревью №2

--- Final_project_of_Sprint_6/src/test/java/tests/BaseTest.java
+++ Final_project_of_Sprint_6/src/test/java/tests/BaseTest.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
-    protected final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
+    static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
 
     @BeforeEach
     public void setUp() {
